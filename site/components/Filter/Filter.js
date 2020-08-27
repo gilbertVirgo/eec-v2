a59a7fef506ffe03d190eb42592e40a36edc6adf
Gil <gilbertjvirgo@gmail.com>
2020-08-27 11:03:44 +0100

    Initial

--- site/components/Filter/Filter.js
+++ site/components/Filter/Filter.js
@@ -1,31 +1,22 @@
+import { Container } from "./styles";
+import InputRange from "react-input-range";
 import React from "react";
-import InputRange from 'react-input-range';
-
-import {
-    Container
-} from "./styles";
 
 export const Filter = ({ children }) => {
+	const [range, setDuration] = React.useState({
+		value: { min: 2, max: 10 },
+	});
 
-    const [range, setDuration] = React.useState({
-        value: { min: 2, max: 10 },
-    });
+	return (
+		<Container>
+			<div>Filters</div>
 
-
-    return (
-        <Container>
-            <div>Filters</div> 
-             
-            <div class="search-container">
-                <img src="" />
-                <input className="input-search" type="text" placeholder="Search by speaker, title or topic" />
-            </div>
-
-            <InputRange
-                maxValue={20}
-                minValue={0}
-                value={range.value}
-                onChange={value => this.setState({ value })} />
-        </Container>
-    );
+			<InputRange
+				maxValue={20}
+				minValue={0}
+				value={range.value}
+				onChange={(value) => this.setState({ value })}
+			/>
+		</Container>
+	);
 };
