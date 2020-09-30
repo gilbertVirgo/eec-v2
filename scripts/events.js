f642ad756c7adc6809132e9d39a24adde221d6e1
Gil <gilbertjvirgo@gmail.com>
2020-09-30 16:24:08 +0100

    Added new query function

--- scripts/events.js
+++ scripts/events.js
@@ -1,16 +1,7 @@
-import Prismic from "prismic-javascript";
+import query from "./query";
 
 export const fetchEvents = async () => {
-	const api = await Prismic.api(
-		"https://eastendchurch.cdn.prismic.io/api/v2",
-		{
-			accessToken:
-				"MC5YekJzU1JJQUFDSUE1UUwx.77-977-9EXtuZyjvv71BNu-_vTTvv73vv73vv711MO-_ve-_ve-_ve-_vX_vv73vv73vv70A77-977-9J0FDMQ",
-		}
-	);
-	let { results } = await api.query(
-		Prismic.Predicates.at("document.type", "event")
-	);
+	let results = await query("event");
 
 	results = results.map(({ data }) => data);
 
@@ -21,3 +12,5 @@ export const fetchEvents = async () => {
 
 	return results;
 };
+
+export const fetchRegularEvents = async () => {};
---
+++ scripts/query.js
@@ -0,0 +1,16 @@
+import Prismic from "prismic-javascript";
+
+export default async (docType) => {
+	const api = await Prismic.api(
+		"https://eastendchurch.cdn.prismic.io/api/v2",
+		{
+			accessToken:
+				"MC5YekJzU1JJQUFDSUE1UUwx.77-977-9EXtuZyjvv71BNu-_vTTvv73vv73vv711MO-_ve-_ve-_ve-_vX_vv73vv73vv70A77-977-9J0FDMQ",
+		}
+	);
+	let { results } = await api.query(
+		Prismic.Predicates.at("document.type", docType)
+	);
+
+	return results;
+};
