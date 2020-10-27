9e6e7e6025c4c0822de22c984c3360dc62138407
Gil <gilbertjvirgo@gmail.com>
2020-10-27 13:24:46 +0000

    Updated ActivityMonitor

--- components/ActivityIndicator/ActivityIndicator.js
+++ components/ActivityIndicator/ActivityIndicator.js
@@ -2,6 +2,7 @@ import { Paragraph } from "..";
 import React from "react";
 import Spinner from "react-loader-spinner";
 import { Wrapper } from "./styles";
+import theme from "../../theme";
 
 export const ActivityIndicator = ({ children, inverted, ...props }) => {
 	const color = inverted ? "white" : "black";
@@ -9,7 +10,9 @@ export const ActivityIndicator = ({ children, inverted, ...props }) => {
 	return (
 		<Wrapper {...props}>
 			<Spinner type="Oval" color={color} width={40} height={40} />
-			<Paragraph style={{ color }}>{children}</Paragraph>
+			<Paragraph style={{ marginTop: theme.gutter + "px", color }}>
+				{children}
+			</Paragraph>
 		</Wrapper>
 	);
 };
--- components/ActivityIndicator/styles.js
+++ components/ActivityIndicator/styles.js
@@ -2,12 +2,25 @@ import styled from "styled-components";
 import theme from "../../theme";
 
 export const Wrapper = styled.div`
-	margin: ${theme.gutter * 2}px 0;
 	display: flex;
 	flex-direction: column;
-	justify-content: space-around;
+	justify-content: center;
 	align-items: center;
-	width: 100%;
-	height: 100px;
-	grid-column: 1 / -1;
+
+	${({ fullScreen }) =>
+		fullScreen
+			? `
+		left: 0; top: 0;
+		width: 100vw;
+		height: 100vh;
+		position: fixed;
+		z-index: 99999;
+		background-color: white;
+		`
+			: `
+		margin: ${theme.gutter * 2}px 0;
+		width: 100%;
+		grid-column: 1 / -1;
+		height: 100px;
+	`}
 `;
